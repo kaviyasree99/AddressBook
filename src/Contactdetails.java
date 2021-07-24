5d4d78aebfb8f02abbc0c309d750ce3a21a27c92
kaviyasree99 <kaviyarajalingam@gmail.com>
2021-07-24 16:44:22 +0530

    Multiple contacts are added

--- src/Contactdetails.java
+++ src/Contactdetails.java
@@ -1,95 +1,91 @@
-
 public class Contactdetails {
-	 String firstName;
-     String lastName;
-     String address;
-     String city;
-     String state;
-     int zipCode;
-     Long phoneNumber;
-     String emailId;
-
-    // access private fields using getters and setters
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public long getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmail(String emailId) {
-        this.emailId =emailId;
-    }
-
-    //Overridden Java toString method
-
-    @Override
-    public String toString() {
-        return "ContactDetails{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zip=" + zipCode +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + emailId + '\'' +
-                '}';
-    }
+	//declaring contact details variables
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zipCode;
+    String phoneNumber;
+    String emailId;
 
 
+   public String getFirstName() {
+       return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+       this.firstName = firstName;
+   }
+
+   public String getLastName() {
+       return lastName;
+   }
+
+   public void setLastName(String lastName) {
+       this.lastName = lastName;
+   }
+
+   public String getAddress() {
+       return address;
+   }
+
+   public void setAddress(String address) {
+       this.address = address;
+   }
+
+   public String getCity() {
+       return city;
+   }
+
+   public void setCity(String city) {
+       this.city = city;
+   }
+
+   public String getState() {
+       return state;
+   }
+
+   public void setState(String state) {
+       this.state = state;
+   }
+
+   public String getZipCode() {
+       return zipCode;
+   }
+
+   public void setZipCode(String newZipcode) {
+       this.zipCode = newZipcode;
+   }
+
+   public String getPhoneNumber() {
+       return phoneNumber;
+   }
+
+   public void setPhoneNumber(String newPhone) {
+       this.phoneNumber = newPhone;
+   }
+
+   public String getEmailId() {
+       return emailId;
+   }
+
+   public void setEmail(String emailId) {
+       this.emailId =emailId;
+   }
+
+   //Overridden Java toString method
+   @Override
+   public String toString() {
+       return "Address Book Contact Details = {" +
+               "First Name='" + firstName + '\'' +
+               ", Last Name='" + lastName + '\'' +
+               ", Address='" + address + '\'' +
+               ", City='" + city + '\'' +
+               ", State='" + state + '\'' +
+               ", Zip=" + zipCode +
+               ", Phone Number='" + phoneNumber + '\'' +
+               ", Email='" + emailId + '\'' +
+               '}';
+   }
 }
